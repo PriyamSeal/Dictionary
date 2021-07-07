f35d763de5327b6df5c06bea0ff764375233d6e7
Priyam Seal <66173287+PriyamSeal@users.noreply.github.com>
2021-07-07 11:25:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -31,5 +31,30 @@ const handle = async (e) => {
       wordEle.innerText = data.title;
       wordMeaning.innerText = data.message;
     }
+
+    if (detectMob) {
+      //your code here
+
+      function updateViewportDimensions() {
+        var w = window,
+          d = document,
+          e = d.documentElement,
+          g = d.getElementsByTagName('body')[0],
+          x = w.innerWidth || e.clientWidth || g.clientWidth,
+          y = w.innerHeight || e.clientHeight || g.clientHeight;
+        return { width: x, height: y };
+      }
+      // setting the viewport width
+      var viewport = updateViewportDimensions();
+
+      function detectMob() {
+        viewport = updateViewportDimensions();
+        if (viewport.width <= 768) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
   }
 };
